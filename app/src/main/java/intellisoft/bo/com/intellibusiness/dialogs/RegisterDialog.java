79ba9ffa64eed1e99585f714efbde6283b96fb77
kevinsubieta <kevinsubieta@gmail.com>
2016-04-10 19:15:37 -0400

    Estilos y selectores en botones.

--- app/src/main/java/intellisoft/bo/com/intellibusiness/dialogs/RegisterDialog.java
+++ app/src/main/java/intellisoft/bo/com/intellibusiness/dialogs/RegisterDialog.java
@@ -3,6 +3,7 @@ package intellisoft.bo.com.intellibusiness.dialogs;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 import intellisoft.bo.com.intellibusiness.R;
@@ -13,7 +14,7 @@ import intellisoft.bo.com.intellibusiness.R;
 public class RegisterDialog extends Dialog {
 
     public RegisterDialog(Context context) {
-        super(context);
+        super(context, android.R.style.Theme_Holo_Light_Dialog_MinWidth);
     }
 
     @Override
@@ -22,5 +23,22 @@ public class RegisterDialog extends Dialog {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setCancelable(false);
         setContentView(R.layout.dialog_register_user);
+        initDialogComponents();
+    }
+
+    private void initDialogComponents(){
+        (findViewById(R.id.btnDialogok)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        (findViewById(R.id.btnDialogCancel)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
     }
 }
