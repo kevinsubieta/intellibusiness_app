049ad6b5eed5ba4013e73fa9f2b11729325203d3
kevinsubieta <kevinsubieta@gmail.com>
2016-05-14 19:59:43 -0400

    Aumentado adapter de carrito y componentes de menú.

---
+++ app/src/main/java/intellisoft/bo/com/intellibusiness/entity/app/Notification.java
@@ -0,0 +1,11 @@
+package intellisoft.bo.com.intellibusiness.entity.app;
+
+import intellisoft.bo.com.intellibusiness.entity.Principal.Entity;
+
+/**
+ * Created by Subieta on 14/05/2016.
+ */
+public class Notification extends Entity {
+
+
+}
